2307ba856d6c67b5121a6be08893ebdf27568747
Raisul-Akram <akramraisul4@gmail.com>
2024-11-21 23:20:06 +0700

    Update quiz.c

--- quiz.c
+++ quiz.c
@@ -123,3 +123,24 @@ void clearScreen() {
     #endif
 }
 
+// Fungsi untuk menampilkan garis pemisah
+void printLine() {
+    printf("==========================================\n");
+}
+
+// Fungsi untuk menampilkan peraturan kuis
+void tampilkanPeraturan() {
+    clearScreen();
+    printLine();
+    printf("           SELAMAT DATANG DI HOLLY WINGS GAME !      \n");
+    printLine();
+    printf("Peraturan:\n");
+    printf("1. Jawab setiap pertanyaan dengan memilih A, B, C, atau D.\n");
+    printf("2. Setiap jawaban benar mendapatkan hadiah.\n");
+    printf("3. Jika salah menjawab, permainan berakhir, dan hadiah Anda akan turun.\n");
+    printf("4. Anda memiliki satu bantuan *50:50* untuk menghilangkan dua opsi yang salah.\n");
+    printLine();
+    printf("Tekan ENTER untuk memulai...\n");
+    getchar();
+}
+
